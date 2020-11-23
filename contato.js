d0c420dc4465a428f178b06fe4a9e3edd0c2589d
joseluiz123 <joseluiz140697@gmail.com>
2020-11-23 10:55:59 -0300

    a

--- contato.js
+++ contato.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Button, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
 
-
 const estilos = StyleSheet.create(
   {
    container: {
