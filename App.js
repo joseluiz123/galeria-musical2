f839201ffe90444792ff16dc8cc7ab2bc7557554
joseluiz123 <joseluiz140697@gmail.com>
2020-11-23 10:47:20 -0300

    commit galeria
    comiti 1

---
+++ App.js
@@ -0,0 +1,222 @@
+//import React, { Component } from 'react';
+import * as React from 'react';
+import { View, ScrollView, Button, StyleSheet, Text, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import './contato';
+import Contato from './contato';
+import './entrevista';
+import Entrevista from './entrevista';
+
+
+const estilos = StyleSheet.create(
+  {
+   container: {
+      flex: 1,
+      //paddingTop: 30,
+      justifyContent: 'flex-end',           
+    },
+
+    titulo_principal: {      
+      backgroundColor: 'white',     
+      alignItems: 'center',
+      alignContent: 'center',
+    },
+
+    rodape: {
+      height: 35,
+      backgroundColor: 'black',     
+      alignItems: 'center',
+      alignContent: 'center',
+    },
+
+    textoLogo: {
+      color: 'white',
+      fontSize: 18,
+      fontWeight: 'bold',
+      alignSelf: 'center',
+      //paddingTop: 8,
+    },
+   
+    textoRodape: {
+      color: 'white',
+      fontSize: 14,
+      fontWeight: 'bold',
+      alignSelf: 'center',
+      paddingTop: 8,
+    },
+
+     menu: {      
+      backgroundColor: 'black',
+      justifyContent: 'space-evenly',
+      flexDirection: 'row',
+      fontSize: 13,  
+      alignItems: 'center',           
+    },
+
+    conteudo: {
+      flex: 1,
+      backgroundColor: '#DCDCDC',
+      justifyContent: 'flex-start',
+    }, 
+
+    titulo:{
+      fontSize: 18,
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+      paddingTop: 8,
+      paddingBottom: 6,
+    },
+
+    subtitulo:{
+      fontSize: 14,
+      paddingTop: 2,
+      paddingBottom: 8,
+      alignSelf: 'center', 
+    },
+
+    imagem:{
+      width: 210,
+      height: 150,
+      alignSelf: 'center',      
+    },
+
+    logo:{
+      width: 300,
+      height: 60,
+     // alignSelf: 'center',      
+    },
+
+    textoBotao: {
+      padding: 15,
+      color: 'white'
+    }, 
+
+  }
+);
+
+function TelaInicial({ navigation }) {
+    return(
+      <View style={estilos.container}> 
+        
+        <View style={estilos.titulo_principal}>    
+          <Image 
+            style={estilos.logo}
+            source={require('./imagens/Logo.png')}                    
+            
+          />
+        </View>
+
+        <View
+            style={{
+              borderBottomColor: 'white',
+              borderBottomWidth: 0.8,
+              paddingTop: 1,
+            }}
+          /> 
+
+        <View style={estilos.menu}>
+            
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.navigate('Contato')}>            
+                <Text style={estilos.textoBotao}>Lançamentos</Text>                      
+            </TouchableHighlight>
+
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.navigate('Entrevista')}>        
+                <Text style={estilos.textoBotao}>Entrevistas</Text>          
+            </TouchableHighlight>
+
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.navigate('Contato')}>          
+                <Text style={estilos.textoBotao}>Resenhas</Text>          
+            </TouchableHighlight>
+
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.navigate('Contato')}>          
+                <Text style={estilos.textoBotao}>Contato</Text>          
+            </TouchableHighlight>
+
+          </View>
+
+      
+      <ScrollView>      
+         
+          <TouchableOpacity onPress={() => navigation.navigate('Contato')}>             
+            <Text style={estilos.titulo}> Última entrevista  </Text>
+         
+            <Image 
+                style={estilos.imagem}
+                source={require('./imagens/cpm.jpg')}                    
+                
+              />
+            <Text style={estilos.subtitulo}> Entrevista com CPM22 </Text>
+          </TouchableOpacity>
+         
+          <View
+              style={{
+                borderBottomColor: 'gray',
+                borderBottomWidth: 1,
+                paddingTop: 6,
+              }}
+            />
+
+          <TouchableOpacity onPress={() => navigation.navigate('Contato')}>             
+            <Text style={estilos.titulo}> Resenha em destaque  </Text>
+         
+            <Image 
+                style={estilos.imagem}
+                source={require('./imagens/acdc.jpg')}                    
+                
+              />
+            <Text style={estilos.subtitulo}> Resenha sobre o ACDC </Text>
+          </TouchableOpacity>
+          
+          <View
+            style={{
+              borderBottomColor: 'gray',
+              borderBottomWidth: 1,
+              paddingTop: 6,
+            }}
+          />  
+
+          <TouchableOpacity onPress={() => navigation.navigate('Contato')}>             
+            <Text style={estilos.titulo}> Resenha em destaque  </Text>
+         
+            <Image 
+                style={estilos.imagem}
+                source={require('./imagens/acdc.jpg')}                    
+                
+              />
+            <Text style={estilos.subtitulo}> Resenha sobre o ACDC </Text>
+          </TouchableOpacity>
+                    
+                            
+          <View style={estilos.rodape}>    
+            <Text style={estilos.textoRodape}>&copy; 2020 Galeria Musical</Text>
+          </View>
+          
+
+          </ScrollView>
+
+        </View>   
+     
+      
+     
+    );
+  }
+
+  const Stack = createStackNavigator();
+
+  function MeuApp() {
+    return(
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Tela Inicial">
+          <Stack.Screen name="Tela Inicial" component={TelaInicial} />
+         
+          <Stack.Screen name="Contato" component={Contato} />
+
+          <Stack.Screen name="Entrevista" component={Entrevista} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+
+export default MeuApp;
---
+++ entrevista.js
@@ -0,0 +1,185 @@
+import React, { Component } from 'react';
+import { View, ScrollView, Button, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
+
+
+const estilos = StyleSheet.create(
+  {
+   container: {
+      flex: 1,      
+      justifyContent: 'flex-end',           
+    },
+
+    titulo_principal: {      
+      backgroundColor: 'white',     
+      alignItems: 'center',
+      alignContent: 'center',
+    },
+
+    textoLogo: {
+      color: 'white',
+      fontSize: 18,
+      fontWeight: 'bold',
+      alignSelf: 'center',
+      paddingTop: 8,
+
+    },
+
+    rodape: {
+      height: 35,
+      backgroundColor: 'black',     
+      alignItems: 'center',
+      alignContent: 'center',
+      marginTop: 20,
+    },
+
+    textoRodape: {
+      color: 'white',
+      fontSize: 14,
+      fontWeight: 'bold',
+      alignSelf: 'center',
+      paddingTop: 8,
+    },
+
+     menu: {      
+      backgroundColor: 'black',
+      justifyContent: 'space-evenly',
+      flexDirection: 'row',
+      fontSize: 13,  
+      alignItems: 'center',
+     
+    },
+
+    conteudo: {
+      flex: 1,
+      backgroundColor: '#DCDCDC',
+      justifyContent: 'flex-start',
+    }, 
+
+    logo:{
+      width: 300,
+      height: 60,
+    },
+
+    textoBotao: {
+      padding: 15,
+      color: 'white'
+    }, 
+
+    inputTexto: {
+      height: 35,
+      width: 250,
+      backgroundColor: 'white',
+      borderColor: 'black',
+      borderWidth: 1,
+      color: 'black',
+      marginLeft: 10,
+      margin: 10,
+      padding: 6,
+    },  
+
+    inputTextArea: {
+      width: 320,
+      backgroundColor: 'white',
+      borderColor: 'black',
+      borderWidth: 1,
+      color: 'black',
+      marginLeft: 10,
+      margin: 10,
+      padding: 6,
+    },
+
+    label: {
+      fontSize: 18,
+      marginLeft: 10,
+      fontWeight: 'bold',
+    },
+
+  }
+);
+
+const Entrevista = ({ navigation }) => { 
+    return(
+      <View style={estilos.container}>   
+        <View style={estilos.titulo_principal}>    
+          <Image 
+            style={estilos.logo}
+            source={require('./imagens/Logo.png')}                    
+            
+          />
+        </View>
+
+        <View
+            style={{
+              borderBottomColor: 'white',
+              borderBottomWidth: 0.8,
+              paddingTop: 1,
+            }}
+          /> 
+
+        <View style={estilos.menu}>
+            
+          <TouchableHighlight underlayColor="orange"  onPress={() => navigation.popToTop()}> 
+                <Text style={estilos.textoBotao}>Início</Text>          
+            </TouchableHighlight>
+
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.popToTop()}>            
+                <Text style={estilos.textoBotao}>Lançamentos</Text>                      
+            </TouchableHighlight>
+
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.popToTop()}>        
+                <Text style={estilos.textoBotao}>Resenhas</Text>          
+            </TouchableHighlight>
+
+            <TouchableHighlight underlayColor="orange" onPress={() => navigation.navigate('Contato')}>          
+                <Text style={estilos.textoBotao}>Contato</Text>          
+            </TouchableHighlight>
+           
+          </View> 
+
+        <ScrollView> 
+          
+          <Text style={{fontSize: 16,
+            margin: 10,
+            fontStyle: 'italic',
+            }}>Para maiores informações sobre o site, anúncios, pautas diversas, envio de materiais, envio de resenhas, promoções, parcerias, divulgação de artistas independentes, colaboração, críticas e etc... Envie-nos um e-mail  </Text>
+
+          <Text style={estilos.label}>Nome:</Text>
+          <TextInput
+           style={estilos.inputTexto}          
+          />
+
+          <Text style={estilos.label}>Email:</Text>
+          <TextInput
+           style={estilos.inputTexto}      
+           keyboardType='email-address'    
+          />
+
+          <Text style={estilos.label}>Assunto:</Text>
+          <TextInput
+           style={estilos.inputTexto}          
+          />
+
+          <Text style={estilos.label}>Mensagem:</Text>
+          <TextInput
+            style={estilos.inputTextArea}          
+            multiline={true}
+            numberOfLines={5}        
+          />
+               
+          <Button
+            title="Enviar"
+            onPress={() => alert('Email enviado')} 
+          />
+
+          <View style={estilos.rodape}>    
+            <Text style={estilos.textoRodape}>&copy; 2020 Galeria Musical</Text>
+          </View>          
+
+          </ScrollView>
+
+        </View>   
+    );
+  }
+
+
+export default Entrevista;
