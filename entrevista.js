8b77b7d782a795c4083ef067d855dd1b0fca1a4c
joseluiz123 <joseluiz140697@gmail.com>
2020-12-01 10:01:09 -0300

    Finalizado

--- entrevista.js
+++ entrevista.js
@@ -175,14 +175,14 @@ const Entrevista = ({ navigation }) => {
               <Image style={estilos.imagens} source={require('./imagens/cpm-entrevista3.jpg')}/>
               <Text style={estilos.nomeArtista}>CPM 22</Text>
               <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
-              <Text style={estilos.descricao}>Por Anderson Nascimento Com mais de 20 anos de estrada a banda paulista CPM22 vive um grande momento artístico....</Text>
+              <Text style={estilos.descricao}>Por Anderson Nascimento. Com mais de 20 anos de estrada a banda paulista CPM22 vive um grande momento artístico....</Text>
             </View>
             
             <View style={estilos.entrevista}>
               <Image style={estilos.imagens} source={require('./imagens/olivia.jpg')}/>
               <Text style={estilos.nomeArtista}>Olivia Genesi</Text>
               <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
-              <Text style={estilos.descricao}>Por Valdir Junior Em meados de 2017, a cantora Olivia Gênesi lançou seu mais novo trabalho, “Amor e Liberdade”,...</Text>
+              <Text style={estilos.descricao}>Por Valdir Junior. Em meados de 2017, a cantora Olivia Gênesi lançou seu mais novo trabalho, “Amor e Liberdade”,...</Text>
             </View>
 
             <View style={estilos.entrevista}>
@@ -196,7 +196,7 @@ const Entrevista = ({ navigation }) => {
               <Image style={estilos.imagens} source={require('./imagens/tao-do-trio.jpg')}/>
               <Text style={estilos.nomeArtista}>TAO DO TRIO</Text>
               <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
-              <Text style={estilos.descricao}>Da Redação Elas se lançaram por nada mais nada menos que um CD cantando Caetano Veloso e hoje já somam...</Text>
+              <Text style={estilos.descricao}>Da Redação: Elas se lançaram por nada mais nada menos que um CD cantando Caetano Veloso e hoje já somam...</Text>
             </View>
 
           </View>             
