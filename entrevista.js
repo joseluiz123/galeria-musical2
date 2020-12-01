8859791ab46205aa5933b303ec07a91cd7fec54e
joseluiz123 <joseluiz140697@gmail.com>
2020-12-01 08:56:57 -0300

    quase lá

--- entrevista.js
+++ entrevista.js
@@ -94,6 +94,39 @@ const estilos = StyleSheet.create(
       fontWeight: 'bold',
     },
 
+    entrevistas: {
+      marginTop: 10,
+      alignItems: 'center'
+    },
+
+    entrevista: {
+      width: 300,
+      borderWidth: 2,
+      borderColor: '#000',
+      backgroundColor: '#fff',
+      marginBottom: 20,
+    },
+
+    imagens: {
+      width: 297,
+      height: 300,
+    },
+
+    nomeArtista: {
+      marginTop: 10,
+      marginLeft: 10,
+      fontWeight: 'bold',
+      fontSize: 20,
+      color: '#518969',
+      textTransform: 'uppercase'
+    },
+
+    descricao: {
+      marginLeft: 10,
+      marginTop: 10,
+      marginBottom: 40
+    }
+
   }
 );
 
@@ -137,39 +170,37 @@ const Entrevista = ({ navigation }) => {
           </View> 
 
         <ScrollView> 
-          
-          <Text style={{fontSize: 16,
-            margin: 10,
-            fontStyle: 'italic',
-            }}>Para maiores informações sobre o site, anúncios, pautas diversas, envio de materiais, envio de resenhas, promoções, parcerias, divulgação de artistas independentes, colaboração, críticas e etc... Envie-nos um e-mail  </Text>
+          {/* Trabalhar aqui */}
+          <View style={estilos.entrevistas}>
+            <View style={estilos.entrevista}>
+              <Image style={estilos.imagens} source={require('./imagens/cpm-entrevista3.jpg')}/>
+              <Text style={estilos.nomeArtista}>CPM 22</Text>
+              <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
+              <Text style={estilos.descricao}>Por Anderson Nascimento Com mais de 20 anos de estrada a banda paulista CPM22 vive um grande momento artístico....</Text>
+            </View>
+            
+            <View style={estilos.entrevista}>
+              <Image style={estilos.imagens} source={require('./imagens/olivia.jpg')}/>
+              <Text style={estilos.nomeArtista}>Olivia Genesi</Text>
+              <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
+              <Text style={estilos.descricao}>Por Valdir Junior Em meados de 2017, a cantora Olivia Gênesi lançou seu mais novo trabalho, “Amor e Liberdade”,...</Text>
+            </View>
 
-          <Text style={estilos.label}>Nome:</Text>
-          <TextInput
-           style={estilos.inputTexto}          
-          />
+            <View style={estilos.entrevista}>
+              <Image style={estilos.imagens} source={require('./imagens/marcela-zanette.jpg')}/>
+              <Text style={estilos.nomeArtista}>MARCELA ZANETTE</Text>
+              <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
+              <Text style={estilos.descricao}>Da Redação: “Esse projeto visa oportunizar o acesso à cultura para pessoas que não costumam frequentar teatros,...</Text>
+            </View>
 
-          <Text style={estilos.label}>Email:</Text>
-          <TextInput
-           style={estilos.inputTexto}      
-           keyboardType='email-address'    
-          />
+            <View style={estilos.entrevista}>
+              <Image style={estilos.imagens} source={require('./imagens/tao-do-trio.jpg')}/>
+              <Text style={estilos.nomeArtista}>TAO DO TRIO</Text>
+              <View style={{ width: 40, height: 10, backgroundColor: '#75d13b', marginLeft: 10 }} />
+              <Text style={estilos.descricao}>Da Redação Elas se lançaram por nada mais nada menos que um CD cantando Caetano Veloso e hoje já somam...</Text>
+            </View>
 
-          <Text style={estilos.label}>Assunto:</Text>
-          <TextInput
-           style={estilos.inputTexto}          
-          />
-
-          <Text style={estilos.label}>Mensagem:</Text>
-          <TextInput
-            style={estilos.inputTextArea}          
-            multiline={true}
-            numberOfLines={5}        
-          />
-               
-          <Button
-            title="Enviar"
-            onPress={() => alert('Email enviado')} 
-          />
+          </View>
 
           <View style={estilos.rodape}>    
             <Text style={estilos.textoRodape}>&copy; 2020 Galeria Musical</Text>
